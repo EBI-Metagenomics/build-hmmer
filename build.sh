8ccd11fd5b879f17a8664d081ef299d775b32cd0
Danilo Horta <danilo.horta@pm.me>
2021-01-18 16:46:01 +0000

    version 3.3.2

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-DIR=hmmer-3.3
+DIR=hmmer-3.3.2
 
 curl http://eddylab.org/software/hmmer/hmmer.tar.gz -o hmmer.tar.gz
+sha256sum --check hmmer.tar.gz.sha256
+
 tar xzf hmmer.tar.gz
 cd $DIR
 ./configure
@@ -13,5 +15,6 @@ cp $DIR/src/hmmscan binhouse/hmmscan
 cp $DIR/src/hmmpress binhouse/hmmpress
 cp $DIR/src/hmmfetch binhouse/hmmfetch
 cp $DIR/src/hmmemit binhouse/hmmemit
+cp $DIR/src/phmmer binhouse/phmmer
 rm -rf $DIR
 ls
